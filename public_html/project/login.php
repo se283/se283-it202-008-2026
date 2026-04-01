7bddd86d198d2d0a444a80b5143b80771ae5465d
se283 <se283@njit.edu>
2026-04-01 15:15:08 -0400

    started milestone1 cleanup - stopped at updating landing.php

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -31,24 +31,21 @@ if (isset($_POST["email"], $_POST["password"])) {
     $hasError = false;
 
     if (empty($email)) {
-        //echo "Email must not be empty<br>";
         flash("Email must not be empty.", "danger");
         $hasError = true;
     }
     // Sanitize and validate email
     $email = sanitize_email($email);
     if (!is_valid_email($email)) {
-        //echo "Invalid email address";
         flash("Invalid email address.", "danger");
         $hasError = true;
     }
     if (empty($password)) {
-        //echo "Password must not be empty<br>";
         flash("Password must not be empty.", "danger");
         $hasError = true;
     }
 
-    if (strlen($password) < 8) {
+    if (!is_valid_password($password)) {
         //echo "Password too short<br>";
         flash("Password must be at least 8 characters long.", "danger");
         $hasError = true;
@@ -70,7 +67,7 @@ if (isset($_POST["email"], $_POST["password"])) {
                         $hash = $user["password"];
                         unset($user["password"]);
                         if (password_verify($password, $hash)) {
-                            //echo "Welcome, $email!<br>";
+                            
                             $_SESSION["user"] = $user; // add the data to the active session
                             die(header("Location: landing.php"));
                         } else {
--- public_html/project/logout.php
+++ public_html/project/logout.php
@@ -1,9 +1,6 @@
 <?php
 // require functions.php to pull in flash()
 require(__DIR__ . "/../../lib/functions.php");
-session_start(); // starts/gets the active session
-session_unset(); // good practice to clear session variables
-session_destroy(); // destroys the session on the server
-session_start(); // start a new session for flash to carry messages
+reset_session(); // clear session data and start a new session
 flash("You have been logged out","success");
-header("Location: login.php"); // redirect back to login
+header("Location: $BASE_PATH/login.php"); // redirect back to login
