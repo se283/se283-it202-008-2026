e190c5e0b3607dd846128982442b98f1e7f089be
se283 <se283@njit.edu>
2026-04-02 19:39:53 -0400

    replaced default UCID with my own UCID on all three php problem files

--- public_html/M6/problem1.php
+++ public_html/M6/problem1.php
@@ -33,7 +33,7 @@ function processBirds($birds) {
     echo "<pre>" . var_export($subset, true) . "</pre>";
    
 }
-$ucid = "mt85"; // replace with your UCID
+$ucid = "se283"; // replace with your UCID
 printHeader($ucid, 1); 
 ?>
 <table>
